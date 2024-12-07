b6ee18bc940c6142084096709af74989d3d94f7c
Vera Freiman <fre-vera@yandex.ru>
2024-12-07 17:30:35 +0300

    Added a general page for todos

---
+++ src/shared/pages/TodosPage/TodosPage.js
@@ -0,0 +1,14 @@
+import { Tasks } from 'widgets';
+
+/**
+ * @function TodosPage
+ * @returns {JSX.Element}
+ */
+
+export const TodosPage = () => {
+  return (
+    <div>
+      <Tasks />
+    </div>
+  );
+};
--- src/shared/pages/index.js
+++ src/shared/pages/index.js
@@ -2,3 +2,4 @@ export { HomePage } from './HomePage';
 export { PhotoPage } from './PhotoPage';
 export { PhotosPage } from './PhotosPage';
 export { TodoPage } from './TodoPage';
+export { TodosPage } from './TodosPage';
