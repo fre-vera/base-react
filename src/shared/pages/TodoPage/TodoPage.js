b29db9eebf06f4e886430d2fd35ea726fbbff635
Vera Freiman <fre-vera@yandex.ru>
2024-12-07 19:29:26 +0300

    Added styles and a function to change colors

--- src/shared/pages/TodoPage/TodoPage.js
+++ src/shared/pages/TodoPage/TodoPage.js
@@ -1,5 +1,8 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useTodosStore } from 'shared/hooks';
+import { getRandomColor } from 'shared/utils';
+import classes from './Todo.module.scss';
 
 /**
  * @function TodoPagePage
@@ -10,12 +13,21 @@ export const TodoPage = () => {
   const params = useParams();
   const todosStore = useTodosStore();
   const todo = todosStore.getTodoById(Number(params.todoId));
+  const [backgroundColor, setBackgroundColor] = useState('');
+
+  useEffect(() => {
+    setBackgroundColor(getRandomColor());
+  }, []);
 
   if (!todo) return <p>Task not found</p>;
 
   return (
-    <div>
-      <h2>{todo.title}</h2>
+    <div className={classes.todoPage}>
+      <div className={classes.todoCard}
+        style={{ background: getRandomColor() }}
+      >
+        <h2 className={classes.todoTitle}>{todo.title}</h2>
+      </div>
     </div>
   );
 };
