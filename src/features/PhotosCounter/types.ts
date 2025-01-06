33e73912ff28e52d0021c74a9a7e03371dc81a85
Vera Freiman <fre-vera@yandex.ru>
2025-01-06 17:05:52 +0300

    Added modal

--- src/features/PhotosCounter/types.ts
+++ src/features/PhotosCounter/types.ts
@@ -1,3 +1,10 @@
-export type PhotosCounterProps = {
-  name: string;
+// Тип для полного объекта поста из API
+export type PostFromAPI = {
+  postId: number;
+  id: number;
+  title: string;
+  body: string;
 };
+
+// Тип для данных формы создания поста
+export type PostFormData = Omit<PostFromAPI, 'postId' | 'id'>;
--- src/features/Posts/Posts.js
+++ src/features/Posts/Posts.js
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import classes from './Posts.module.scss';
 import { Card } from 'entity';
+import { AddPost } from './AddPost';
 
 /**
  * @typedef {import('./types').PostsProps} PostsProps
@@ -10,16 +12,45 @@ import { Card } from 'entity';
  * @param {PostsProps} props
  * @returns {JSX.Element}
  */
-
 export const Posts = (props) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
-    <ul className={classes.posts}>
-      {props.posts.map((posts) => (
-        <Card.Post key={posts.id}
-          post={posts}
-        />
-      ))}
-    </ul>
+    <div className={classes.postsContainer}>
+      {/* Кнопка добавления поста */}
+      <button className={classes.button} onClick={handleOpenModal}>
+        Add Post
+      </button>
+
+      {/* Список постов */}
+      <ul className={classes.posts}>
+        {props.posts.map((post) => (
+          <Card.Post key={post.id} post={post} />
+        ))}
+      </ul>
+
+      {/* Модальное окно для добавления поста */}
+      {isModalOpen && (
+        <div className={classes.modalOverlay}>
+          <div className={classes.modalContent}>
+            <AddPost />
+            <button
+              className={classes.closeButton}
+              onClick={handleCloseModal}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
